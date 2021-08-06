4b1c687aa3453882db5563dd2fe1ccec52945031
wangchunhao <w0000ff@163.com>
2021-08-06 15:49:03 +0800

    login

---
+++ src/components/home.vue
@@ -0,0 +1,15 @@
+<template>
+<el-button type="info" @click="loginOut">退出</el-button>
+</template>
+<script>
+export default {
+  methods: {
+    loginOut () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
 
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   router,
